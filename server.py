4477411222dcbe15969f4423ff513a4de9641e94
Dan Rowe <draco2003@gmail.com>
2019-03-26 23:59:47 +0000

    Use global session to reuse requests connection

--- server.py
+++ server.py
@@ -8,26 +8,17 @@ load_dotenv()
 USERNAME = os.getenv("USERNAME")
 APIKEY = os.getenv("APIKEY")
 
+# Setup request session and use session to send auth and header along with every request
+session = requests.Session()
+session.auth = (USERNAME, APIKEY)
+session.headers = {'user-agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Mobile Safari/537.36'}
 
-def get_models(username, api_key, sku=None):
-    """ Send a GET request to Wayfair 3D model API to get all 3D models
 
-        Args:
-        username: API username
-        api_key : API key
-        sku .   : Product SKU
-        Returns:
-        A JSON response for 3D model information
-    """
-
-    url = "https://wayfair.com/3dapi/models"
-
-    # use session to send auth and header along with every request
-    session = requests.Session()
-    session.auth = (username, api_key)
-    session.headers = {'user-agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Mobile Safari/537.36'}
+def get_models(sku=None):
+    url = "https://www.wayfair.com/3dapi/models"
     params = None
 
+    # simple example so we aren't handling multiple skus
     if sku:
         params = {'skus[]': sku}
 
@@ -41,13 +32,13 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     # Send a GET request to Wayfair 3D model API. Returns JSON for 3D models
-    products = get_models(USERNAME, APIKEY)
+    products = get_models()
     return render_template('index.html', products=products)
 
 
 @app.route("/product/<sku>")
 def product(sku):
-    products = get_models(USERNAME, APIKEY, sku)
+    products = get_models(sku)
     product = products[0]
     # Fix glb url until it gets fixed on the server side
     product['model']['glb'] = product['model']['glb'].replace('http://img', 'https://secure.img1')
